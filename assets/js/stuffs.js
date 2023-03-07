84f535ad438109992dfbe06ea1cf1948162a3885
Giarlan Santana <giarlansilva@yahoo.com>
2023-03-07 19:05:31 -0300

    'JSon' file coding.

---
+++ assets/js/stuffs.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Tshirt', img:'../images/img1.png', price:39.90, sizes:['P', 'M', 'G'], description:'Tshirt feminina cor de rosa com estampa'},
+    {id:2, name:'Camisa', img:'../images/img2.png', price:69.99, sizes:['P', 'M', 'G'], description:'Camisa pollo masculina de manga longa na cor preta'},
+    {id:3, name:'Camiseta', img:'../images/img2.png', price:49.90, sizes:['P', 'M', 'G'], description:'Camiseta masculina na cor branca'},
+    {id:4, name:'Camisa', img:'../images/img2.png', price:69.99, sizes:['P', 'M', 'G'], description:'Camisa pollo masculina de manga longa na cor verde'},
+    {id:5, name:'Bermuda', img:'../images/img2.png', price:49.00, sizes:['P', 'M', 'G'], description:'Bermuda feminina na cor Old Rose'},
+    {id:6, name:'Bermuda', img:'../images/img2.png', price:56.50, sizes:['P', 'M', 'G'], description:'Bermuda masculina na cor bege'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Saia com cinto de couro sintético cor de abóbora'}
+];
