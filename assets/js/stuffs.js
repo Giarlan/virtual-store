3e02b19a57a163a1664d4893fd218dc7596ef840
Giarlan Santana <giarlansilva@yahoo.com>
2023-03-09 23:04:20 -0300

    CHANGE 'STYLE.CSS' AND 'STUFFS.JS' FILE
    * Style: change header backgroun-color and add two more images on the page body('img8.png' and 'img9.png')
    * Feat: add 'id:8' and 'id:9' on Json file to insert two more images on the page body

--- assets/js/stuffs.js
+++ assets/js/stuffs.js
@@ -5,5 +5,8 @@ let stuffJson = [
     {id:4, name:'Camisa', img:'assets/images/img4.png', price:69.99, sizes:['P', 'M', 'G'], description:'Camisa pollo masculina de manga longa na cor verde'},
     {id:5, name:'Bermuda', img:'assets/images/img5.png', price:49.00, sizes:['P', 'M', 'G'], description:'Bermuda feminina na cor Old Rose'},
     {id:6, name:'Bermuda', img:'assets/images/img6.png', price:56.50, sizes:['P', 'M', 'G'], description:'Bermuda masculina na cor bege'},
-    {id:7, name:'Minissaia', img:'assets/images/img7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Saia com cinto de couro sintético cor de abóbora'}
+    {id:7, name:'Minissaia', img:'assets/images/img7.png', price:72.00, sizes:['P', 'M', 'G'], description:'Saia com cinto de couro sintético cor de abóbora'},
+    {id:8, name:'Camisa', img:'assets/images/img8.png', price:59.90, sizes:['P', 'M', 'G'], description:'Camisa Pollo masculina lilás'},
+    {id:9, name:'Camiseta', img:'assets/images/img9.png', price:56.90, sizes:['P', 'M', 'G'], description:'Camiseta Pollo feminina vermelha'}
+
 ];
