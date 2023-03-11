d72d297e6783aacb8ce5a95fa920fc0e9174eeac
Giarlan Santana <giarlansilva@yahoo.com>
2023-03-10 23:33:59 -0300

    ADD TWO MORE ACTIONS IN JS CODE TO GIVE INTERACTION FOR ITEMS
    12 – Feat: add a JavaScript code line with a function that blocks the natural page refresh action when clicking on the item
    14 – Feat: insert on the same function a click event that shows a 'modal area' on the page body
    132 – Fix : correct a bug because of an error on the name of class

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,15 @@ const qsa = (el) => document.querySelectorAll(el);
 
 stuffJson.map ((item, index) => {
     let stuffItem = qs ('.type .stuff-item').cloneNode(true);
+
     stuffItem.querySelector('.stuff-item-img img').src = item.img;
     stuffItem.querySelector('.stuff-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     stuffItem.querySelector('.stuff-item-name').innerHTML = item.name;
     stuffItem.querySelector('.stuff-item-desc').innerHTML = item.description;
-
-
+    stuffItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+        qs('.stuff-window-area').style.display = 'flex';
+        })
 
     qs('.stuff-area').append(stuffItem);
 });
